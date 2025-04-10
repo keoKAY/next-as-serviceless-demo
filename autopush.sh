e10d5a13710de3d1c9924e0e486c8e4a8b55b935
KAY KEO <keokay888@gmail.com>
2025-04-10 08:54:20 +0700

    update the autopush script

--- autopush.sh
+++ autopush.sh
@@ -1,9 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 message="$1"
 if [ -z "$1" ]; then 
     echo "Please provide a commit message." 
     exit 1 
 fi
+
 git add . 
 git commit -m "$message " 
-git push origin $(git branch --show-current)
+git push origin "$(git branch --show-current)"
